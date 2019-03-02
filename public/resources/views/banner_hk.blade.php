170add3c118da2d0bb73d2a41329495af1f56e82
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-03-02 19:45:26 +0530

    Home Banner
    Video Slides removed

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -44,42 +44,13 @@
                     </a>
                 </div>
             </div>
-            <div class="carousel-item ">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item  d-flex align-items-center">
-                        <video class="leadin-video video-bgv1" muted autoplay playsinline>
-                            <source src="/videos/bgv1.mp4" type="video/mp4">
-                        </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'liber') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item d-flex align-items-center">
-                        <video class="leadin-video video-bgv2" muted playsinline>
-                            <source src="/videos/bgv2.mp4" type="video/mp4">
-                        </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'liber') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  
 
         </div>
         <ol class="carousel-indicators">
             <li data-target="#home_banner" data-slide-to="0" class="active"></li>
             <li data-target="#home_banner" data-slide-to="1"></li>
-            <li data-target="#home_banner" data-slide-to="2"></li>
-            <li data-target="#home_banner" data-slide-to="3"></li>
+
         </ol>
     </div>
 </section>
