51703f7232620c352a11165aca672fc79b211820
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-03-07 15:32:22 +0530

    Update hindu.blade.php

--- public/resources/views/hindu.blade.php
+++ public/resources/views/hindu.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <main>
         <section style="padding-top: 50px">
-            <iframe src="http://hindumecca.in/index.php" style="width: 100%; height: 100%">
+            <iframe src="http://hindumecca.in" style="width: 100%; height: 100%">
 
             </iframe>
         </section>
