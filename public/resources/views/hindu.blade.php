5de8a2cc588adfcce09e8a9d8ec9f0166e2a5ec8
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-03-07 15:29:26 +0530

    Update hindu.blade.php

--- public/resources/views/hindu.blade.php
+++ public/resources/views/hindu.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <main>
         <section style="padding-top: 50px">
-            <iframe sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src="http://hindumecca.in/index.php" style="width: 100%; height: 100%">
+            <iframe src="http://hindumecca.in/index.php" style="width: 100%; height: 100%">
 
             </iframe>
         </section>
