ca8d1f3a633322438178d62005577e3c1da9044b
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-03-23 01:20:51 +0530

    Update Kernel.php

--- public/app/Http/Kernel.php
+++ public/app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        
     ];
 
     /**
@@ -63,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle'             => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'auth.avita'           => \App\Http\Middleware\AvitaAuthMiddleware::class,
         'api_service_provider' => \App\Http\Middleware\APIServiceProviderMiddleware::class
+        'cors'                 => \Barryvdh\Cors\HandleCors::class, // add this line to enable cors to your routes
     ];
 }
