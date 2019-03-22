93e4caf7c2d6b022b3015f9eb3eb4f78c954d4d6
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-03-22 14:00:13 +0530

    Update magus.blade.php

--- public/resources/views/product/magus.blade.php
+++ public/resources/views/product/magus.blade.php
@@ -12,8 +12,8 @@
         <section class="product-liber-banner">
             <div class="responsive-block">
                 <div class="banner-block responsive-item">
-                    <div class="banner-bg hidden-sm-down" style="background-image: url('/images/banner/magus_banner.jpg')"></div>
-                    <div class="banner-bg hidden-md-up" style="background-image: url('/images/banner/magus_banner_mob.jpg')"></div>
+                    <div class="banner-bg hidden-sm-down" style="background-image: url('/images/banner/banner.jpg')"></div>
+                    <div class="banner-bg hidden-md-up" style="background-image: url('/images/banner/banner_mob.jpg')"></div>
                     <div class="banner-info">
                         <div class="btn-group mt-3">
                             {{--<img class="mx-auto" src="/images/liber/brand.png">--}}
