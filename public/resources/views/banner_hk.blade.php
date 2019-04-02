2f21c76ef41b3947edeccc23e8ce28967db5691a
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-04-02 15:21:28 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -2,9 +2,22 @@
     <div id="home_banner" class="carousel slide" data-ride="carousel" data-interval="0" data-pause="">
         <div class="carousel-inner" role="listbox">
 
+        <div class="carousel-item  active">
+                <div class="responsive-block">
+                    <div class="banner-block responsive-item d-flex align-items-center">
+                        <video class="leadin-video video-bgv2" muted playsinline>
+                            <source src="/videos/magus.mp4" type="video/mp4">
+                        </video>
+                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
+                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
+                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'magus') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-
-            <div class="carousel-item active">
+            <div class="carousel-item">
                 <div class="responsive-block">
                     <div class="banner-block responsive-item">
                         <a href="#">
@@ -22,24 +35,7 @@
                 </div>
             </div>
 
-            <div class="carousel-item ">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item">
-                        <a href="/campus">
-                            @if( App::isLocale('en') )
-                                <div class="banner-bg hidden-sm-down"
-                                     style="background-image: url('/images/banner/campus_banner.jpg')"></div>
-                            @else
-                                <div class="banner-bg hidden-sm-down"
-                                     style="background-image: url('/images/banner/campus_banner.jpg')"></div>
-                            @endif
-                            <div class="banner-bg hidden-md-up"
-                                 style="background-image: url('/images/banner/banner_mob.jpg')"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
+           
         <div class="carousel-item">
                 <div class="responsive-block">
                     <div class="banner-block responsive-item">
@@ -58,31 +54,11 @@
                 </div>
             </div>
 
-            <div class="carousel-item">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item">
-                        <a href="https://paytmmall.com/shop/search?q=avita&from=organic&child_site_id=6&site_id=2">
-                            @if( App::isLocale('en') )
-                                <div class="banner-bg hidden-sm-down"
-                                     style="background-image: url('/images/banner/website_banner.png')"></div>
-                            @else
-                                <div class="banner-bg hidden-sm-down"
-                                     style="background-image: url('/images/banner/website_banner.png')"></div>
-                            @endif
-                            <div class="banner-bg hidden-md-up"
-                                 style="background-image: url('/images/banner/mobile_banner.png')"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-
-            <ol class="carousel-indicators">
+           
+              <ol class="carousel-indicators">
                 <li data-target="#home_banner" data-slide-to="0" class="active"></li>
                 <li data-target="#home_banner" data-slide-to="1"></li>
                 <li data-target="#home_banner" data-slide-to="2"></li>
-              <li data-target="#home_banner" data-slide-to="3"></li>
-
              </ol>
         </div>
 </section>
