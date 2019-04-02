b3867ab38f92731e1f21c2b8b445a79db57ed25b
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-04-02 15:31:54 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -8,12 +8,7 @@
                         <video class="leadin-video video-bgv2" muted playsinline>
                             <source src="/videos/magus.mp4" type="video/mp4">
                         </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'magus') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
+                       </div>
                 </div>
             </div>
 
@@ -84,9 +79,14 @@
 
         var imageTimer = null;
 
+        $('video').on('ended', function(){
+            $('#home_banner').carousel('next');
+        });
         $('#home_banner').on('init slide.bs.carousel', function (e) {
 
             clearTimeout( imageTimer );
+
+            $(this).find('video')[0].pause();
 
             var new_video = $(e.relatedTarget).find('video')[0];
 
@@ -96,7 +96,7 @@
 
                 imageTimer = setTimeout( function( ){
                     $('#home_banner').carousel('next');
-                }, 4000 );
+                }, 5000 );
 
             }
         }).trigger('init');
