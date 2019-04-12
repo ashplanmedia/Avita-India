b374a44a9b6d375f101259f0ae2b17665ca0a552
Sandeep Rawat <40665342+sandy4u1994@users.noreply.github.com>
2019-04-12 22:55:22 +0530

    Update support.js

--- js/support.js
+++ js/support.js
@@ -8,7 +8,7 @@ var $productConfiguartionField = $("#product_config_field");
 // 1. Product Type
 $productTypeField.on('init', function() {
 
-    var url = "https://avita.global/api/products/types/";
+    var url = "https://asp.avita.global/product/types";
 
     $.getJSON( url ).done(function ( data ) {
 
