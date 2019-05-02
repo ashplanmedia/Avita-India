de9d8801fa554fe7adbd4e8cc0343730d134b46b
Sandeep Rawat <sandy4u1994@gmail.com>
2019-05-03 01:01:25 +0530

    Contact Us Page

--- public/resources/views/pages/contact_us_sg.blade.php
+++ public/resources/views/pages/contact_us_sg.blade.php
@@ -6,41 +6,52 @@
 
 @section('content')
 
-<main>
+    <main>
 
-    <section class="aboutus-panel top-nav-padding ls-0">
-        <div class="aboutus-contact py-5 px-2">
-            <div class="container">
-                <h1 class="section-title mt-0 mb-5 my-md-5 font-weight-light">@lang('site.contactus_contactus') </h1>
+        <section class="aboutus-panel top-nav-padding ls-0">
+            <div class="aboutus-contact py-5 px-2">
 
-                <div class="row">
-                    <div class="col-12 col-md-6 mb-5 mb-md-0 ls-0">
-                        <div class="contact_us_card mb-4 mb-md-5">
-                            <h3 class="contact_us_title">Repair Service and Technical support – Singapore</h3>
+                <div class="container">
+                    <h1 class="section-title mt-0 mb-5 my-md-5 font-weight-light">@lang('site.contactus_contactus') <hr/></h1>
 
-                            <div class="support_tel">800-188-8608</div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h2 class="contact_us_title">General Sales Enquiry :</h2>
 
-                            <div class="mb-2"><b>Email:</b><br>
-                                sgsupport@avita.com</div>
+                            <div class="mb-2">+91-9840024085,<br/> +91-11-6635-1144</div>
+
+                            <div class="mb-2"><b>Email :</b><br>
+                                INsales@nexstgo.com</div>
 
                             <div><b>@lang('site.contactus_operating')</b><br>
-                                Mon-Fri 09:00-18:00, Sat 09:00-13:00</div>
+                                Monday to Friday : 9 am - 6 pm</div>
+
+
+                            <div class="mb-2"><b>Address :</b><br>
+                                211B - 211C, 2nd Floor, Elegance Tower, Jasola Vihar, New Delhi, Delhi-110025</div>
+                            <h2 class="contact_us_title">For Service :</h2>
+                            <div class="mb-2">Toll Free : 1800-22-3902</div>
+
+                        </div>
+                        <div class="col-sm-6">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.0533242758497!2d77.2863191150805!3d28.538116882455064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce6aa9cfc4f99%3A0x665909b7d4962e0c!2sElegance+Tower%2C+Jasola+Vihar%2C+New+Delhi%2C+Delhi+110025!5e0!3m2!1sen!2sin!4v1556101695940!5m2!1sen!2sin" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+
                         </div>
                     </div>
                 </div>
 
+
             </div>
+        </section>
+
+        <div class="gotop-wrap">
+            <button class="btn-gotop"><span class="sr-only">Back to Top</span></button>
         </div>
-    </section>
-
-    <div class="gotop-wrap">
-        <button class="btn-gotop"><span class="sr-only">Back to Top</span></button>
-    </div>
 
 
 
 
 
-</main>
+    </main>
 
 @endsection
