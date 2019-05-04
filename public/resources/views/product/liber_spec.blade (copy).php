368a853e1d9efa57661fa3bc9194a76c8d672ca2
Sandeep Rawat <sandy4u1994@gmail.com>
2019-05-04 17:49:58 +0530

    Tech spec heading changed

--- public/resources/views/product/liber_spec.blade (copy).php
+++ public/resources/views/product/liber_spec.blade (copy).php
@@ -15,59 +15,58 @@
             <div class="nav-product-wrap">
                 <div class="container px-0">
                     <nav class="nav nav-pills nav-product-spec justify-content-center">
-                        <a class="col text-center nav-link" data-toggle="tab" href="#spec-1" role="tab">
-                            <div class="spec-item-name mb-4"><div class="d-sm-block">AVITA LIBER New Generation </div><div class="d-sm-block"> (13.3-inch)</div></div>
-                            <img class="hidden-sm-down" src="/images/liber/13/silver.png">
-                            <ul class="list-unstyled spec-color-list d-flex flex-wrap align-items-center justify-content-center mt-4 ">
-
-                                <li style="background-color: #7d7e82" class="active" data-image="/images/liber/13/silver.png"></li>
-                                <li style="background-color: #c5c2d9" data-image="/images/liber/13/lightpurple.png"></li>
-                                <li style="background-color: #3173b6" data-image="/images/liber/13/blue.png"></li>
-                                <li style="background-color: #6c55a4" data-image="/images/liber/13/purple.png"></li>
-                                <li style="background-color: #81b9db" data-image="/images/liber/13/lightblue.png"></li>
-                                <li style="background-color: #cf346d" data-image="/images/liber/13/pink.png"></li>
-                                <li style="background-color: #7fb041" data-image="/images/liber/13/green.png"></li>
-                                <li style="background-color: #e1d059" data-image="/images/liber/13/yellow.png"></li>
-                                <li style="background-color: #f58847" data-image="/images/liber/13/orange.png"></li>
-                                <li style="background-color: #c1c0b8" data-image="/images/liber/13/lightsilver.png"></li>
-                                <li style="background-color: #dcdde2" data-image="/images/liber/13/white.png"></li>
-                                <li style="background-color: #bea98d" data-image="/images/liber/13/gold.png"></li>
-                                <li style="background-color: #1b1b1d" data-image="/images/liber/13/black.png"></li>
-                                <li style="background-color: #bdbbb7" data-image="/images/liber/13/lightsilver.png"></li>
-
-                            </ul>
-                        </a>
-                        <a class="col text-center nav-link" data-toggle="tab" href="#spec-2" role="tab">
-                            <div class="spec-item-name mb-4"><div class="d-sm-block">AVITA LIBER New Generation </div><div class="d-sm-block">(14-inch)</div></div>
-                            <img class="hidden-sm-down" src="/images/liber/14/silver.png">
-                            <ul class="list-unstyled spec-color-list d-flex flex-wrap align-items-center justify-content-center mt-4">
-
-                                <li style="background-color: #7d7e82" class="active" data-image="/images/liber/14/silver.png"></li>
-                                <li style="background-color: #c5c2d9" data-image="/images/liber/14/lightpurple.png"></li>
-                                <li style="background-color: #3173b6" data-image="/images/liber/14/blue.png"></li>
-                                <li style="background-color: #6c55a4" data-image="/images/liber/14/purple.png"></li>
-                                <li style="background-color: #81b9db" data-image="/images/liber/14/lightblue.png"></li>
-                                <li style="background-color: #cf346d" data-image="/images/liber/14/pink.png"></li>
-                                <li style="background-color: #7fb041" data-image="/images/liber/14/green.png"></li>
-                                <li style="background-color: #e1d059" data-image="/images/liber/14/yellow.png"></li>
-                                <li style="background-color: #f58847" data-image="/images/liber/14/orange.png"></li>
-                                <li style="background-color: #c1c0b8" data-image="/images/liber/14/lightsilver.png"></li>
-                                <li style="background-color: #dcdde2" data-image="/images/liber/14/white.png"></li>
-                                <li style="background-color: #bea98d" data-image="/images/liber/14/gold.png"></li>
-                                <li style="background-color: #1b1b1d" data-image="/images/liber/14/black.png"></li>
-                                <li style="background-color: #bdbbb7" data-image="/images/liber/14/lightsilver.png"></li>
-                            </ul>
-                        </a>
-
                         <a class="col text-center nav-link active" data-toggle="tab" href="#spec-3" role="tab">
-                            <div class="spec-item-name mb-4"><div class="d-sm-block">AVITA LIBER</div><div class="d-sm-block">(12.5-inch) @if( !$HAVE_LIBER_12) / @lang('site.coming_soon') @endif</div></div>
+                            <div class="spec-item-name mb-4"><div class="d-sm-block">AVITA LIBER</div><div class="d-sm-block">12.5" (31.75 cm) @if( !$HAVE_LIBER_12) / @lang('site.coming_soon') @endif</div></div>
                             <img class="hidden-sm-down" src="/images/liber/12/pink.png">
                             <ul class="list-unstyled spec-color-list d-flex flex-wrap align-items-center justify-content-center mt-4 ">
                                 <li style="background-color: #DE9299" class="active" data-image="/images/liber/12/pink.png"></li>
-                                <li style="background-color: #BAA1B9" data-image="/images/liber/12/purple.png"></li>
+                                 <li style="background-color: #BAA1B9" data-image="/images/liber/12/purple.png"></li>
                                 <li style="background-color: #55665B" data-image="/images/liber/12/green.png"></li>
                                 <li style="background-color: #74AACA" data-image="/images/liber/12/cyan.png"></li>
-
+                               
+                            </ul>
+                        </a>
+                        <a class="col text-center nav-link" data-toggle="tab" href="#spec-1" role="tab">
+                            <div class="spec-item-name mb-4"><div class="d-sm-block">AVITA LIBER</div><div class="d-sm-block">13.3" (33.78 cm)</div></div>
+                            <img class="hidden-sm-down" src="/images/liber/13/silver.png">
+                            <ul class="list-unstyled spec-color-list d-flex flex-wrap align-items-center justify-content-center mt-4 ">
+                                
+        <li style="background-color: #7d7e82" class="active" data-image="/images/liber/13/silver.png"></li>
+        <li style="background-color: #c5c2d9" data-image="/images/liber/13/lightpurple.png"></li>
+        <li style="background-color: #3173b6" data-image="/images/liber/13/blue.png"></li>
+        <li style="background-color: #6c55a4" data-image="/images/liber/13/purple.png"></li>
+        <li style="background-color: #81b9db" data-image="/images/liber/13/lightblue.png"></li>
+        <li style="background-color: #cf346d" data-image="/images/liber/13/pink.png"></li>
+        <li style="background-color: #7fb041" data-image="/images/liber/13/green.png"></li>
+        <li style="background-color: #e1d059" data-image="/images/liber/13/yellow.png"></li>
+        <li style="background-color: #f58847" data-image="/images/liber/13/orange.png"></li>
+        <li style="background-color: #c1c0b8" data-image="/images/liber/13/lightsilver.png"></li>
+        <li style="background-color: #dcdde2" data-image="/images/liber/13/white.png"></li>
+        <li style="background-color: #bea98d" data-image="/images/liber/13/gold.png"></li>
+        <li style="background-color: #1b1b1d" data-image="/images/liber/13/black.png"></li>         
+        <li style="background-color: #bdbbb7" data-image="/images/liber/13/lightsilver.png"></li>
+                                
+                            </ul>
+                        </a>
+                        <a class="col text-center nav-link" data-toggle="tab" href="#spec-2" role="tab">
+                            <div class="spec-item-name mb-4"><div class="d-sm-block">AVITA LIBER</div><div class="d-sm-block">14" (35.56 cm)</div></div>
+                            <img class="hidden-sm-down" src="/images/liber/14/silver.png">
+                            <ul class="list-unstyled spec-color-list d-flex flex-wrap align-items-center justify-content-center mt-4">
+                                                        
+        <li style="background-color: #7d7e82" class="active" data-image="/images/liber/14/silver.png"></li>
+        <li style="background-color: #c5c2d9" data-image="/images/liber/14/lightpurple.png"></li>
+        <li style="background-color: #3173b6" data-image="/images/liber/14/blue.png"></li>
+        <li style="background-color: #6c55a4" data-image="/images/liber/14/purple.png"></li>
+        <li style="background-color: #81b9db" data-image="/images/liber/14/lightblue.png"></li>
+        <li style="background-color: #cf346d" data-image="/images/liber/14/pink.png"></li>
+        <li style="background-color: #7fb041" data-image="/images/liber/14/green.png"></li>
+        <li style="background-color: #e1d059" data-image="/images/liber/14/yellow.png"></li>
+        <li style="background-color: #f58847" data-image="/images/liber/14/orange.png"></li>
+        <li style="background-color: #c1c0b8" data-image="/images/liber/14/lightsilver.png"></li>
+        <li style="background-color: #dcdde2" data-image="/images/liber/14/white.png"></li>
+        <li style="background-color: #bea98d" data-image="/images/liber/14/gold.png"></li>
+        <li style="background-color: #1b1b1d" data-image="/images/liber/14/black.png"></li>         
+        <li style="background-color: #bdbbb7" data-image="/images/liber/14/lightsilver.png"></li>
                             </ul>
                         </a>
                     </nav>
@@ -122,7 +121,7 @@
                                 <div>299 x 199.5 x 14.89</div>
                             </li>
                             <li class="spec-item d-flex">
-                                <div class="offset-md-1 col-4 col-md-3">Weight</div>
+                            <div class="offset-md-1 col-4 col-md-3">Weight</div>
                                 <div>1.14 kg</div>
 
                             </li>
@@ -136,9 +135,9 @@
                             </li>
                             <li class="spec-item d-flex">
                                 <div class="offset-md-1 col-4 col-md-3">Colour</div>
-                                <div>Princess Pink, Fragrant Lilac, Peacock Green, Angel Blue.</div>
+    <div>Princess Pink, Fragrant Lilac, Peacock Green, Angel Blue.</div> 
                             </li>
-
+                            
                         </ul>
                     </div>
                 </div>
@@ -201,9 +200,9 @@
                             </li>
                             <li class="spec-item d-flex">
                                 <div class="offset-md-1 col-4 col-md-3">Colour</div>
-                                <div>Avita Purple, Pearl White, Champagne Gold, Princess Pink, Cloud Silver, Shamrock Green, Fragrant Lilac, Space Grey, Angel Blue, Sunflower Yellow, Matt Black, Sunrise Orange, Peacock Green, Urban Ruby. </div>
+                               <div>Avita Purple, Pearl White, Champagne Gold, Princess Pink, Cloud Silver, Shamrock Green, Fragrant Lilac, Space Grey, Angel Blue, Sunflower Yellow, Matt Black, Sunrise Orange, Peacock Green, Urban Ruby. </div>
                             </li>
-
+                            
                         </ul>
                     </div>
                 </div>
@@ -266,7 +265,7 @@
                             </li>
                             <li class="spec-item d-flex">
                                 <div class="offset-md-1 col-4 col-md-3">Colour</div>
-                                <div>Avita Purple, Pearl White, Champagne Gold, Princess Pink, Cloud Silver, Shamrock Green, Fragrant Lilac, Space Grey, Angel Blue, Sunflower Yellow, Matt Black, Sunrise Orange, Peacock Green, Urban Ruby. </div>
+<div>Avita Purple, Pearl White, Champagne Gold, Princess Pink, Cloud Silver, Shamrock Green, Fragrant Lilac, Space Grey, Angel Blue, Sunflower Yellow, Matt Black, Sunrise Orange, Peacock Green, Urban Ruby. </div>
                             </li>
 
                         </ul>
