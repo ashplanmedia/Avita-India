83d28179f9cbb6cd6f8546039211c9a4f0facee8
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-04 19:56:32 +0530

    Update contact_us.blade.php

--- public/resources/views/pages/contact_us.blade.php
+++ public/resources/views/pages/contact_us.blade.php
@@ -23,8 +23,8 @@
                             <div class="mb-2"><b>Email :</b><br>
                                 insales@nexstgo.com</div>
 
-                            <div><b>@lang('site.contactus_operating')</b><br>
-                                Monday to Friday : 9 am - 6 pm</div>
+                            <div><b>@lang('site.contactus_operating') :</b><br>
+                                Monday to Friday : 09:00 am - 06:00 pm</div>
 
 
                             <div class="mb-2"><b>Address :</b><br>
