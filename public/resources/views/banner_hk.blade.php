78304d1fb1357aa866b5a9d02938bf7f116c9885
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-06 16:26:52 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -1,7 +1,23 @@
 <section class="top-nav-padding homepage-banner">
     <div id="home_banner" class="carousel slide" data-ride="carousel" data-interval="0" data-pause="">
         <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
+        
+     <div class="carousel-item  active">
+                <div class="responsive-block">
+                    <div class="banner-block responsive-item">
+                        @if( App::isLocale('en') )
+                            <div class="banner-bg hidden-sm-down"
+                                 style="background-image: url('/images/banner/Web_Banner.png')"></div>
+                        @else
+                            <div class="banner-bg hidden-sm-down"
+                                 style="background-image: url('/images/banner/Web_Banner.png')"></div>
+                        @endif
+                        <div class="banner-bg hidden-md-up"
+                             style="background-image: url('/images/banner/Mobile_Banner.png')"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
                 <div class="responsive-block">
                     <div class="banner-block responsive-item">
                         <a href="/product/magus12-2in1-laptop">
@@ -44,23 +60,6 @@
                     </div>
                 </div>
             </div>
-
- <div class="carousel-item">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item">
-                        @if( App::isLocale('en') )
-                            <div class="banner-bg hidden-sm-down"
-                                 style="background-image: url('/images/banner/Web_Banner.png')"></div>
-                        @else
-                            <div class="banner-bg hidden-sm-down"
-                                 style="background-image: url('/images/banner/Web_Banner.png')"></div>
-                        @endif
-                        <div class="banner-bg hidden-md-up"
-                             style="background-image: url('/images/banner/Mobile_Banner.png')"></div>
-                    </div>
-                </div>
-            </div>
-
 
             <ol class="carousel-indicators">
                 <li data-target="#home_banner" data-slide-to="0" class="active"></li>
