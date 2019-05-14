c5ed509919970fd5553d97c1e4ecaa7aabe69b6d
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-14 07:21:55 -0700

    Update web.php

--- public/routes/web.php
+++ public/routes/web.php
@@ -170,7 +170,7 @@ if ( Request::segment(1) != 'admin') {
     //});
 
     Route::get('/product/{slug}', 'ProductsController@showProductFeatures')->name('product.overview');
-    Route::get('/product/{slug}/specNew', 'ProductsController@showProductSpecNew')->name('product.spec-new');
+    Route::get('/product/{slug}/spec_new', 'ProductsController@showProductSpecNew')->name('product.spec_new');
     Route::get('/product/{slug}/spec', 'ProductsController@showProductSpec')->name('product.spec');
     Route::get('/product/{slug}/support', 'ProductsController@showProductSupport')->name('product.support');
     Route::get('/product/{slug}/where_to_buy', 'ProductsController@whereToBuy')->name('product.map');
