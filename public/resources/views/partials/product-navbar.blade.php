fd55e90c77c59d8f5ab625437fe8b3228a4422c3
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-14 06:47:23 -0700

    Update product-navbar.blade.php

--- public/resources/views/partials/product-navbar.blade.php
+++ public/resources/views/partials/product-navbar.blade.php
@@ -12,10 +12,10 @@
                 @else
 
                     <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.overview', 'liber12') }}">@lang('site.productnav_overview_12')</a></li>
-                    <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.overview', 'liber') }}">Overview AVITA LIBER 13.3" & 14" New Generation</a></li>
+                    <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.overview', 'liber') }}">Overview AVITA LIBER 13.3" & 14"</a></li>
                 @endif
 
-                <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.spec-old', 'liber') }}">@lang('site.productnav_spec')</a></li>
+                <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.spec-liber', 'liber') }}">@lang('site.productnav_spec')</a></li>
              
 
                 <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.support', 'liber') }}">@lang('site.support')</a></li>
