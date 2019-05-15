f560da8b1d05928e42fdfadc2c7a45765e70d074
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-15 08:11:17 -0700

    Update SendInMail.php

--- public/app/Mail/SendInMail.php
+++ public/app/Mail/SendInMail.php
@@ -15,8 +15,8 @@ class SendInMail {
     {
 
     	
-		$this->url =env("SENDIN_URL","https://api.sendinblue.com/v2.0");
-		$this->api_key =env("SENDIN_API_KEY","7T3v62rhFjV1UzyI");
+		$this->url ="https://api.sendinblue.com/v2.0";
+		$this->api_key ="7T3v62rhFjV1UzyI";
         $this->mailin = new Mailin($this->url,$this->api_key);
     }
 
