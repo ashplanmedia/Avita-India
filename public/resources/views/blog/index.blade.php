95ea96e374515e1f8ae9641fbe9d5c0e1a3ffb81
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-16 04:33:33 -0700

    Update index.blade.php

--- public/resources/views/blog/index.blade.php
+++ public/resources/views/blog/index.blade.php
@@ -10,6 +10,26 @@
         <section class="focus-event top-nav-padding">
             <div class="container">
                 <h3 class="section-title ls-0 my-5">@lang('site.blogs_title')</h3>
+		<div class="row">
+                    <!-----
+        ------->
+                    <div class="col-12 col-md-6">
+                        <div class="event-item mb-5">
+                            <a href="/ssd-storage-for-modern-laptops">  <p>Why SSD Storage has become a Necessity for Modern Computing?</p></a><br/>
+                            <figure>
+                                <div class="image-wrap">
+                                    <img style="width: 100%; height:100%;" src="/images/blog/blog7.jpg" />
+                                </div>
+                                <figcaption class="d-flex event-caption coming_soon px-4 py-1">
+                                    <div class="event-date"><i class="fa fa-calendar-o mr-3" aria-hidden="true"></i><strong>Date:</strong> 16th May, 2019</div>
+                                    <div class="event-status coming_soon ml-auto"></strong>Category:</strong>SSD Fast Storage</div>
+                                </figcaption>
+                            </figure>
+
+                        </div>
+
+                    </div>
+		    </div>
                 <div class="row">
                     <!-----
         ------->
