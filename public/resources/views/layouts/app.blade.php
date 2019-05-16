6192ea0236c5813db36a101f591bab01c706f517
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-16 20:29:36 +0530

    Update app.blade.php

--- public/resources/views/layouts/app.blade.php
+++ public/resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <meta property="og:title" content="@yield('title')">
     <meta property="og:description" content="{{ $description }}">
 
-    <meta property="og:image" content="{{ asset('images/Web_Banner_LIBER_1200x630.png') }}">
+    <meta property="og:image" content="{{ asset('images/AVITA INDIA.png') }}">
     <meta property="og:image:type" content="image/jpeg">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
