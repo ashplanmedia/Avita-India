e02218126905043122a6baf5cb91d2529de11b11
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-20 20:32:03 +0530

    Update liber_feature.blade.php

--- public/resources/views/product/liber_feature.blade.php
+++ public/resources/views/product/liber_feature.blade.php
@@ -43,7 +43,7 @@
                     <div class="banner-info">
                         <div class="ac-computer-wrap">
                             <img class="ac-computer-image ac-computer-1" src="/images/liber/sc2_img1.png">
-                            <!-- <img class="ac-computer-image ac-computer-2" src="/images/liber/sc2_img2.png"> -->
+                          <img class="ac-computer-image ac-computer-2" src="/images/liber/AVITA_Backpack.png"> 
                         </div>
                         <div class="h2 banner-header mb-4 mb-sm-5">Inside-out Vibrancy</div>
                         <div class="banner-para">
