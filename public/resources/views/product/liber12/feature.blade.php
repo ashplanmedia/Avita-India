997c74d342671c341ede6eb0427a2e3ff0b6a17f
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-21 20:06:18 +0530

    Update feature.blade.php

--- public/resources/views/product/liber12/feature.blade.php
+++ public/resources/views/product/liber12/feature.blade.php
@@ -62,7 +62,7 @@
                      <!---   <img class="ac-computer-image ac-computer-2" src="/images/liber/liber12_feature/sc2_img2.png">--->
                         </div>
 						<div class="row">
-						<div class="col-9">
+						<div class="col-8">
                         <div class="h2 banner-header mb-4 mb-sm-5">Inside-out Vibrancy</div>
                         <div class="banner-para">
 							
@@ -74,7 +74,7 @@
 						</div>			
 					
 						</div>
-							<div class="col-3">
+							<div class="col-4">
 			 <img class="ac-computer-image ac-computer-1" src="/images/liber/AVITA_Backpack.png">
 
 							</div>
