7f5319ecfacb71ec5b95b24db898cdd5b1e337bc
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-25 17:42:07 +0530

    Update index.blade.php

--- public/resources/views/blog/index.blade.php
+++ public/resources/views/blog/index.blade.php
@@ -156,7 +156,7 @@
 
                     </div>
                
-                    </div>
+                 
                     <div class="row">
                     <div class="col-12 col-md-6">
                         <div class="event-item mb-5">
