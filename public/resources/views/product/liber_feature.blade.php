0a5585e947febd0d2e8672b30ce4c52087dabcd4
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-27 12:09:27 +0530

    Update liber_feature.blade.php

--- public/resources/views/product/liber_feature.blade.php
+++ public/resources/views/product/liber_feature.blade.php
@@ -454,7 +454,7 @@
                     <li>Models or specifications may vary from country to country. Check with your local distributors or retailers for any updates on the current product.</li>
                     <li>Weights vary depending on configuration and manufacturing variability.</li>
                     <li>Colors of actual products may differ from product shots due to photography lighting or display setting of your viewing device.</li>
-					<li>We try our best to provide accurate and complete product information online yet we reserve the rights to keep, change or correct any information without further notice.</li>
+		<li>We try our best to provide accurate and complete product information online yet we reserve the rights to keep, change or correct any information without further notice.</li>
                     <li>Windows is either registered trademark or trademark of Microsoft Corporation in the United States and/or other countries.</li>
                 </ul>
             </div>
