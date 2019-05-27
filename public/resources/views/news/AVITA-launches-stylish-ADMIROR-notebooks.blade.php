db9d12dcb060db031c5211d1967de536ff82a1f8
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-27 11:14:06 +0530

    Update AVITA-launches-stylish-ADMIROR-notebooks.blade.php

--- public/resources/views/news/AVITA-launches-stylish-ADMIROR-notebooks.blade.php
+++ public/resources/views/news/AVITA-launches-stylish-ADMIROR-notebooks.blade.php
@@ -10,63 +10,52 @@
 
     <section class="focus-event top-nav-padding">
         <div class="container">
-            <h3 class="section-title ls-0 my-5">@lang('site.blog1')</h3>
+            <h3 class="section-title ls-0 my-5">#YouCanBe AVITA launches stylish ADMIROR notebooks</h3>
             <div class="row">
-          <!-----
-            ------->
-        <div class="col-12">
-    <ul style="list-style-type: none;">
-        <li>  What’s the best thing technology does?<br/></li>
-        <li> It brings people together.<br/></li>
-        <li> So, here you are spending your time on our blog post, trying to find the best answers to your queries.<br/></li>
-        <li>This is not just a ‘One way street’.<br/></li>
-        <li>What’s happening right now is a communication between you and our creation.<br/></li>
-        <li>And an effective communication is one of the most basic developments that build your college life.<br/></li>
-        <li>And that’s exactly where technology steps in. <br/></li>
-        <li>Today, we will take you through our guide to help you sail through one of the most difficult decisions every student has to take in his college life – buying a Student Laptop. <br/></li>
-        <li>So, without any further ado, let’s dive in.<br/></li>
-        </ul>
-    <h2>What are the Key features Students should look in a laptop?</h2>
-<h3>Weight</h3> <p>Weight is determined largely by screen size -- each added inch brings added ounces of plastic and metal to support it. 
-What's the sweet-spot for laptop weight? Most buying guides say it's about 4 lbs, give or take.</p>
-<h3>Screen Size</h3>
-<p>First, know that improving display technology (HD, Full HD, 4K, etc.) has enabled impressive view ability in all laptops. 
-Students who mostly read and write can utilize smaller screens while those whose learning involves more visual or creative activities can benefit from larger ones.</p>
-<h3>Battery Life</h3>
-<p>These days, if a laptop manufacturer isn't offering battery life of 8-12+ hours, you should probably keep looking.</br>
-Consider these factors:</p>
-<ul>
-    <li>How big is it?</li> 
-	<li>How will you use it?</li> 
-	<li>Where will you use it?</li> 
-	</ul>
-<h3>Processor, RAM, and Storage</h3>
-<p>Storage is less important today. RAM shopping is easy: 4 GB is the minimum, 8 GB is considered typical, and the ability to add more RAM chips is a useful option. 
-But it is processor (CPU) power that truly matters from an educational/work product perspective, since it determines how much your laptop can accomplish for you (and how fast).</p>
+       <div class="col-12">
+	   <h6>Architectural aesthetic-inspired designs and elevated performance on the go</h6>
+   Taiwan, 27th May 2019 — Fashionable tech brand AVITA announces its plan to launch the stylish notebook series ADMIROR in second quarter this year. Bringing innovative technology, fashionable design and gothic architecture elements into one, ADMIROR is the perfect “fashion gadget” at work and for daily entertainment enjoyment. ADMIROR notebooks combine a number of professional features in a vibrant design resplendent in five color options. Breaking through the norm and shattering the dull image of traditional notebooks, ADMIROR seeks to become the ultimate fashionable daily companion for stylish users looking for a chic, highly portable and affordable premium notebook solution. 
 
-<h3>Graphic Capabilities</h3>
-<p>You'll have to choose between systems with integrated graphics or ones with dedicated graphics cards, which are placed separately on the motherboard and are typically more powerful. 
-Most student users will be satisfied with integrated graphics, but dedicated GPUs might be best for those who intend to also play advanced computer games or whose course of study (such as engineering, videography, etc.) involves the creation or rendering of complex images or artwork.</p>
+#YouCanBe: Gothic architectural aesthetic in 5 colors with ULTRA Slim design
+With the theme of “#YouCanbe”, AVITA continues to break the traditional market perception of lightweight notebooks with its new ADMIROR series, available in a range of color options that contrast the norm: “Blazing Brown” and “Flaming Copper”, as well as “Delight Pink”, “Pride Blue” and “Traveler Green”. The five colors create five unique overall characters to match the user’s own personal sense of self. 
 
-<h3>Ports and Accessories</h3>
-<p>When it comes to USBs, the more the better, especially with all of the peripherals, such as smartphones, that a student might need to plug in. 
-The same goes for external connection options (HDMI, DVI-D, USB Type-C); the more you have, the greater your flexibility. </p>
-<h3>Wireless Connectivity</h3>
-<p>Once an option, screaming fast wireless is now essential in student laptop -- not just for research but for lifestyle activities such as movie streaming and social media. 
-The latest wireless standard to look for is 802.11ac, which has a theoretical maximum of 1300 megabits per second (Mbps), approx. three times that of the previous consumer standard, 802.11n.</p>
-<h3>Durability</h3>
-<p>Laptops with added features for hard use or rugged environments are already popular in industrial settings and are quickly moving into the student market, too. 
-Models with spill-proof keyboards (in which any small amounts of liquid are directed away from the interior electrical components) are ideal for students and available in every price category. 
-More expensive models might use magnesium alloy like the AVITA Liber to protect against short drops or falls, and some laptops (and laptop cases) are put through military-level durability tests (MIL-SPEC 810G, for example).
-<h3>Technical Support</h3>
-<p>Depending on the student's technical know-how, the quality and availability of technical support from the laptop manufacturer could be an important factor, so consider checking the online forums where these matters are discussed. 
-If you truly need a new student laptop to last 4-5 years, consider the cost-benefit value of any extended warranty that might be offered.
-There you go.
-These were the top factors every student should consider when buying a student laptop.</p>
+As the ultimate gadget for fashionistas, AVITA ADMIROR combines hints of Gothic architecture portrayed via contemporary aesthetics, realizing a seamless and slim body design with the most delicately crafted details. It is designed with rounded corners and concealed hinges, taking inspirations from epoch-making architectural art, whilst applying innovative technology to bring a refreshingly extraordinary user appearance. The vault design, which is situated between the screen and the keyboard, further pushes its Gothic styling theme to the forefront.
 
-        </div>
-        </div>
-        </div>
+AVITA ADMIROR also adopts ULTRA Slim design and a special metallic chassis. With its slimmest point at just 9mm thin and weighing from just 1.29kg, ADMIROR is extremely handy to carry as your always-on life partner. This series is also available in two different screen size options – 14-inch and 15.6-inch models, fulfilling the needs of a wide spectrum of user types.
+
+ 
+#YouCanBe: High performance, supremely multi-functional: brings a whole new meaning to the “Fashion Gadget” market
+As AVITA’s hero product series, ADMIROR’s ‘upgraded version’ elevated feature set includes a 4K ULTRA HD IPS display that offers natural color reproduction and rich color range, together with 4 ways passive boosters and 2 ways full range speakers to boost audio sound quality, bringing the best audio-visual experience at work or for movies and music enjoyment on-the-go. Its touchscreen allows users to freely navigate the web while its extra-large touch panel supports multi-touch control, significantly enhancing overall flexibility and accuracy. Its backlit keyboard also enhances the overall user experience enormously. ADMIROR’s battery life lasts for up to 10 hours, and it supports Windows Hello, a biometric identity and access control feature that incorporates fingerprint reading for strengthened privacy protection. To ensure a thoroughly lightweight design, ADMIROR is equipped with two USB-C ports, and an adapter * to meet users’ needs to charge devices whilst out and about, transfer data or connect to different devices to extend versatility.
+*Adapter includes USB-C and USB, HDMI and 3.5mm headphone jack
+
+ADMIROR builds on AVITA LIBER’s standout award pedigree: LIBER wins ‘Best Choice Award - Jury’s Special Award’
+The AVITA LIBER series, which consists of 14 distinctive colors and 6 special print designs, received overwhelming consumer support and critical acclaim after its launch. As the first of AVITA’s new notebook series, AVITA LIBER changed public perception of what a notebook can do, winning ‘Best Choice Award - Jury’s Special Award’ in a further measure of its success, as AVITA continues to be the first choice innovator in “customized” notebook design.
+
+Launch date and Suggested retail price: 
+ADMIROR will be launched in Hong Kong in August 2019 and other countries later on. Suggested retail price to be released later.
+
+###
+About AVITA
+AVITA is a consumer lifestyle brand found in the United States. In 2016, Nexstgo introduced AVITA to Asia, and since has been promoting the brand and making its products available to international consumers.The AVITA product series with ranges for PC, Smart home and IoT devices has three core values: Liberty, Diversity and Individualism for an exceptional and highly personal experience for the new generation of tech users.
+www.avita.global 
+
+About Nexstgo Company Limited
+Established in 2016, Nexstgo Company Limited was co-founded by Mr. Wilson Leung, CEO, Alco Holdings Limited (SEHK: 0328), and Mr. Alex Chung, out of a shared passion and pursuit of quality tech-innovation. Nexstgo is committed to bringing new revolutionary experiences in technology through introducing a wide product range, which includes PC, Smart home, IoT, artificial intelligence and corporate solutions. Nexstgo is growing steadily in the Asia Pacific region. In August 2016, its Hong Kong headquarters and Taiwan Design Centre were set up. In just two years, the company has expanded its business to 12 areas, and is fully committed to the development of the Tech Market.
+www.nexstgo.com 
+
+
+For media queries, please contact:
+SPRG
+Veronica Li +852-9414-2862 / +852-2114-4945 / veronica.li@sprg.com.hk
+Ian Yung +852-9035-5313 / +852-2114-4958 / ian.yung@sprg.com.hk
+
+Nexstgo
+Carol So +852-9016-1919 / carol.so@nexstgo.com
+
+   
+   </div>
+   </div>
+   </div>
         </section>
         <div class="gotop-wrap">
             <button class="btn-gotop"><span class="sr-only">Back to Top</span></button>
