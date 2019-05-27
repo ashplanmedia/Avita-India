a5fc6ca4760fbb7a52195c1d972f775fa8e54e88
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-05-27 11:56:12 +0530

    Update feature.blade.php

--- public/resources/views/product/liber12/feature.blade.php
+++ public/resources/views/product/liber12/feature.blade.php
@@ -62,7 +62,7 @@
                      <!---   <img class="ac-computer-image ac-computer-2" src="/images/liber/liber12_feature/sc2_img2.png">--->
                         </div>
 						<div class="row">
-						<div class="col-8">
+						
                         <div class="h2 banner-header mb-4 mb-sm-5">Inside-out Vibrancy</div>
                         <div class="banner-para">
 							
@@ -71,13 +71,10 @@
                             <span class="d-lg-block">Where the form of a laptop remains,</span>
                             <span class="d-lg-block">it on top features chic and interchangeable </span>
                             <span class="d-lg-block">form factors to reveal your true colors.</span>
-						</div>			
+									
 					
 						</div>
-							<div class="col-4">
-			 <img class="ac-computer-image ac-computer-1" src="/images/liber/AVITA_Backpack.png">
-
-							</div>
+							
 							</div>
                     </div>
 				</div>
