763927c0c2c9913c86dcfcf3234efb9eec40cbaa
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-06-04 14:11:39 +0530

    Update blog8.blade.php

--- public/resources/views/blog/blog8.blade.php
+++ public/resources/views/blog/blog8.blade.php
@@ -1,53 +1,67 @@
-@extends('layouts.app')
+ @extends('layouts.app')
 
 @section('title')
-    @lang('site.blogs_title')
+     @lang('site.blogs_title')
 @stop
 
 @section('content')
     <main>
-
-
-        <section class="focus-event top-nav-padding">
-            <div class="container">
-                <h3 class="section-title ls-0 my-5">@lang('site.blog8')</h3>
-                <div class="row">
-                    <!-----
-                      ------->
-                    <div class="col-12">
-
-                            Millennials need their own space.
-This statement not just applies to their lifestyle but also aptly fits with their computing needs.
-Although today's computer systems provide an ample amount of storage that people do not worry about it anymore, the question – Is storage a major factor in choosing a certain laptop or PC, still remains.
-If your answer to the above question is YES, we recommend you to keep reading because, today, you are about to find out all the extra features of Cloud Storage that you can use to your benefit.
-Let’s cut to the chase and dive straight in!
-Here are the factors one must underscore while considering Cloud Storage.
-<h2>1.  Affordability</h2>
-Money has been, and always will be, a prime factor when considering any technology product. The buzz is investing your money in the right place is the first step towards personal success. Cloud services are available at very affordable rates which allow the premium to be paid annually or monthly so you can choose the plan which suits you individually. This also helps in choosing features that are needed by you and pay just for them. Instead of relying on CDs or a website download to install software on each device, you’ll have the software that is ready to use at fair rates or free of any charges.
-<h2>2.  No tech skills required</h2>
-The neck wrecking problem with almost every new technology is its increasing complexity. Cloud storage has an easy to use online interface which does not 
-
-require you to have any additional skills. You can simply upload your data in the Cloud storage and fetch it wherever you want. It's that simple.
-<h2>3.  Use on-the-go</h2>
-A significant majority of millennials do not only work in offices. They work from their home, hotels, and coffee shops. With the help of Cloud storage, they enable themselves to access their files anywhere in real time. Cloud computing is perfect for people who are always on the go. In addition to that, you can even fetch your files on different devices which just might leave you with a habit of ditching your laptop computers while traveling anywhere.
-<h2>4.  Backup for your personal files</h2>
-Breakage, loss or theft of computer devices is a common problem today. This leads to disappointment in owners of such devices, and the disappointment comes due to wasted money, and then because of lost data. This can also lead to loss of valuable personal data which can be heart-breaking. Save yourself some sleepless nights by switching to Cloud storage.  All cloud storages have an auto-upload/auto-sync function that helps you automatically synchronize all data and download at will.
-<h2>5.  Provides Quality and Structure Control</h2>
-No matter what you do, consistency is essential for your progress. That is why we recommend using a cloud-based system because all documents are stored in one place and in a single format. With accessing the same information in the same format, you can maintain consistency in data, avoid human error, and have a clear record of any revisions or updates.
-<h2>6.  Automatic Software Updates</h2>
-For those who have a lot to get done, there isn't anything more irritating than having to wait for a system update to get installed. Cloud-based applications automatically refresh and update themselves, which can save you a lot of time.
-OVER TO YOU
-As we are at the end of the discussion, we hope that the above-mentioned features of Cloud Storage will surely be your saviour in times of needs. All 
-
-
-these benefits of Cloud storage are just a decision away from you. Go grab them and be as efficient as possible.
-
-
+        <section class="event-detail top-nav-padding">
+            <div class="container mb-5">
+                <br/>
+               <div class="event-banner">
+                        <img src="images/blog/ssd-storage-for-modern-laptops.png" class="img-responsive" />
                     </div>
+                  
+               
+                <div class="event-desc-block pb-5">
+                        <p class="event-desc-content">
+                            </p><p class="MsoNormal" style="text-align: center; line-height: 20.0pt; mso-line-height-rule: exactly;" align="center"><strong><span lang="EN-US" style="font-size: 18.0pt; mso-bidi-font-family: 'Times New Roman';">@lang('site.blog8')</span></strong></p>
 
-                </div>
-            </div>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> The Hard Disk Drive technology has conquered a major percentage of the market share, for a better part of the last two decades. Being a predominant storage device for modern computers, the HDD technology has lured the computer enthusiasts for a very long time. 
+But not anymore, modern storage technology like Solid State Drive has surfaced as a prime challenger to the existing non-volatile storage mediums.
+In this blog post, we are going to mention the major benefits of SSD as it slowly replaces the majority of hard disk drives. So, let’s consider the following factors:
+</span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>1. Access Time</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">A Solid State Drive ensures an access speed of 35 to 100 microseconds. This speed is 100 times faster than that of Hard Disk Drive. What does that mean for you? An SSD can help users run programs more quickly. Any program that requires a large amount of data can run smoothly using SSD storage. If you want your laptop to boot-up within 10-13 seconds, SSD storage is the way to go. Therefore, SSDs ensure a minimal boot time for accessing your personal computer.</span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>2. Reliability</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> One of the most important aspects of an SSD is its simple built. A hard disk drive generally has mechanical parts which are subject to various issues in the long run. On the other hand, an SSD has no mechanical parts, providing a better performance and reliability. </span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>3. Capacity </strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">When it comes to capacity, SSDs sport a storage space up to 2TB. Though being on the higher side when it comes to price tag, Solid State Drives give a tough competition to Hard Disk Drives when it comes to storage capacity.</span></p>
+
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>4. Power</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">Solid State Drives use flash technology which reduces the power usage to a huge extent. It not only results in a lower energy bill, but also an increase in battery life for laptops.  </span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>5. Noise</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> Hard Disk Drives are known to create noises while functioning. The reason is the mechanical parts wearing down over time. The noise created by Hard Disk Drives is very annoying and can hamper your computing experience. Solid State Drives have no mechanical parts and therefore do not produce any noise while functioning.</span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>6. Size</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">Solid State Drives are available in the market in 2.5”, 1.8”, and 1.0” size. On the other hand, Hard Disk Drives usually come in 3.5” and 2.5” both for laptops and desktop computers. There’s no option smaller than these two. With SSDs, modern computing machines enjoy the opportunity to be ultra-light and slim.</span></p>
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>7. Heat</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">The amount of heat generated in Hard Disk Drives can slowly damage the functionality due to increase in wear and damage. On the other hand, Solid State Drives generate less heat, contributing to its increased lifespan and durability.</span></p>
+
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>8. Price Range</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">SSD is pricier than traditional hard drives, one of the reasons why budget or cheap laptops don’t have it. Although in today’s world, most users use SSD laptops along with an external hard disk for all storage other than system applications. But with a Solid State Drive, you get all the above advantages which definitely make it worth the premium you pay for it.</span></p>
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>Over to You</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">There is no reason why any user would want to drive a brand new car with an old engine. So why would you buy a laptop with an old-school mechanical hard drive and merge onto the superfast informational highway? Check out the brand new AVITA laptops sporting the lightning-fast SSD storage only on www.avita-india.com!</span></p>
+
+       </div>
         </section>
+
         <div class="gotop-wrap">
             <button class="btn-gotop"><span class="sr-only">Back to Top</span></button>
         </div>
@@ -59,8 +73,8 @@ these benefits of Cloud storage are just a decision away from you. Go grab them 
 @section('js')
 
     <script>
-
-        $('.fb_share_btn').click( function(e) {
+    
+  $('.fb_share_btn').click( function(e) {
 
             e.preventDefault();
 
@@ -71,6 +85,7 @@ these benefits of Cloud storage are just a decision away from you. Go grab them 
 
             });
         });
+        
 
         $('.twitter_share_btn').click(function(e){
 
