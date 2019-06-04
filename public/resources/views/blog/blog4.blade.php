c703fe326dfa3970a3f1f18bbfae3e274896049b
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-06-04 13:27:11 +0530

    Update blog4.blade.php

--- public/resources/views/blog/blog4.blade.php
+++ public/resources/views/blog/blog4.blade.php
@@ -6,85 +6,69 @@
 
 @section('content')
     <main>
-    
+        <section class="event-detail top-nav-padding">
+            <div class="container mb-5">
+                <br/>
+               <div class="event-banner">
+                        <img src="/images/blog/blog2.jpg" class="img-responsive" />
+                    </div>
+                  
+               
+                <div class="event-desc-block pb-5">
+                        <p class="event-desc-content">
+                            </p><p class="MsoNormal" style="text-align: center; line-height: 20.0pt; mso-line-height-rule: exactly;" align="center"><strong><span lang="EN-US" style="font-size: 18.0pt; mso-bidi-font-family: 'Times New Roman';">@lang('site.blog4')</span></strong></p>
 
-    <section class="focus-event top-nav-padding">
-        <div class="container">
-            <h3 class="section-title ls-0 my-5">@lang('site.blog4')</h3>
-            <div class="row">
-          <!-----
-            ------->
-        <div class="col-12">
-    <p>Technology has evolved all around the corners of the world. Surviving in the 21st century without laptops and smartphones is impossible. Each and every person carries a laptop to the office or college for their  specific purposes.</p>
-<h3 style="text-align:center">Just imagine a day without a laptop in your office or college.</h3>    
-<br/>
-<ul>
-<li>How would you be providing presentations?</li>    
-<li>How would you be able to carry on with your work?</li>    
-<li>How would you be able to perform accurate calculations?</li>    
-<li>How would you be preparing the content for your project?</li>    
-<li>Most importantly, how would you be playing games?</li>    
-
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"><ul><li>Weight is one of the most important health concerns people face today. </li>
+<li>Most of us have heard the warnings that several common causes of early death, such as heart disease, stroke, type 2 Diabetes, and even certain types of cancer are linked to obesity. </li>
+<li>While weight management involves a whole lifestyle of good habits, one integral piece is weighing yourself. </li>
+<li>You need a smart scale!</li>
+<li>When you're not happy with your body or your weight or your progress in managing it, stepping on the scale sucks. </li>
+<li>We want to convince you that getting a smart bathroom scale, one that's wirelessly connected to an app that helps you track your weight, is not only going to help monitor all the pounds in your household, but also has the potential to make your whole family happier about doing it. </li>
 </ul>
-    <p>
-You cannot imagine.
-The most important factor to consider while buying a laptop is that it must match with your personality. Laptops are available all over the world in various models as well as multiple sizes along with different specifications.
-Imagine yourself carrying a poorly designed laptop in the college. Would that be a good first impression?
-Now imagine yourself carrying a laptop that is stylish as well as attractively colored. How's that for a first impression?
-    
-    
+</span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>What is a Smart Scale?</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">A smart body scale is an electronic bathroom scale that measures weight and other body composition metrics, and then sends them wirelessly to an app or online service that automatically records the data.</span></p>
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>What Do Smart Scales Do?</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">The primary job of a smart scale is to record your weight and other metrics in an app that lets you see your history of weight change over time.<br/>
+Multiple people can use the same scale, making them perfect for a health-conscious family. <br/>
+Most scales differentiate each person based on their prior readings. <br/>
+If one family member weighs around 160 pounds and the other weighs 140, the scale quickly figures out who is who. <br/>
+If two people have close readings, the scale or app usually asks the person being weighed at the moment to confirm his or her identity. <br/>
+A smart scale helps a family organize their health initiatives because it makes the tracking effortless. <br/>
+No one ever has to write down the details of the weigh-in because the tracking happens automatically. <br/>
+The most you have to do is make sure the scale's batteries are still going strong.<br/>
+</span></p>
+
+
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> <strong>What are the benefits of Using a Smart Scale?</strong></span></p>
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';">Beyond tracking weight, there are other benefits to using a smart scale:
+<ul type="1">
+<li>You don't have to look! Stepping on the scale sometimes sucks. When you use a smart scale, you don't have to look if you don't want to. The weight will still be recorded in your app, and you or your doctor can look later. Long-term trends are at least as important as the daily numbers.</li>
+
+<li>Pregnancy and post-pregnancy weight-tracking is much easier. A lot of people don't like facing the numbers of their weight, but imagine how much worse it could be if your body's hormones are fluctuating during or after a pregnancy. This also disables the body composition features, which pregnant women (as well as people with implanted devices, such as pacemakers) are often told not to use.</li>
+
+<li>Smart scales can increase motivation. If you and your family like technology, incorporating a smart scale might actually make it fun to start tracking weight, or at least motivate them to start. Having a new toy in the house could give everyone the boost they need to start stepping on the scale every day.</li>
+
+<li>Kids can learn good habits. If everyone steps on the scale every day and there's a record to prove it, kids will pick up on what it means to create a habit out of their weight-management practices, and creating good habits is key.
+</li>
+<li>They typically pair with fitness trackers. If you already use a fitness tracker, buying the companion smart scale will give you much more insight into how your activities, sleep, and food intake affect your weight.</li>
+</ul>
+</span></p>
+
+<p class="MsoNormal" style="text-align: justify; text-justify: inter-ideograph; line-height: 16.0pt; mso-line-height-rule: exactly;"><span lang="EN-US" style="mso-bidi-font-size: 12.0pt; mso-bidi-font-family: 'Times New Roman';"> We hope these benefits push you forward to get yourself a smart scale. If you are looking forward to buy one, don’t forget to check out the brand new AVITA Smart Scale!</span>
+
 </p>
-    <h3 style="text-align:center">Why Avita?</h3>    
-    <p>We, here at Avita know the importance of carrying around a laptop that matches your personality. People in the office or college judge you by the laptop model you carry along with you. We believe in the wonder as well as the power of an individual. When it comes to us, we believe in providing the most innovative as well as technologically advanced and stylish product to our customers.</p>
-    
-        <h3 style="text-align:center">Avita Liber</h3>    
-    <p>Available in various colors namely Angel Blue, Blossom Pink, Fragrant Lilac, Himalayan Blue, Peacock Green and Silver, Avita Liber is everything one could dream about. The laptop is slim as well as light weighted which would allow you to carry it anyplace without any strain on your arms or shoulders.  
-What is the point of purchasing a laptop if it just does not match your requirements?
-</p>
-    
-    
-<h3 style="text-align:center">Let us explain the features of Avita Liber.</h3>  <ul>
-    <li>Avita Liber is equipped with a high-end processor Core i5 along with 8GB RAM and 128GB SSD which ensures that the laptop does not compromise its quality irrespective of its thin build and lightweight. </li>
-    <li>Multiple color options allow the user to select a laptop that suits his/her personality.</li>
-    <li>The, as well as the thin build of the laptop, boosts the personality of the person carrying it along. Additionally, the light weight of the laptop prevent the users from loading strain on their arms or shoulders.</li>
-    <li>The laptop is equipped with premium, oversized glass trackpad. Surrounded by an elegant diamond cut bevelling, 	the premium oversized trackpad allows for easy gesture controls.</li>
-    <li>The feature to lock and unlock the laptop at the tip of a finger is the most comforting of them all.</li>
-    <li>If you are buying this product, then you do not need to worry about battery life.</li>
-    </ul>
-      
-      
-<h3 style="text-align:center">Specifications of Avita Liber</h3> 
-<table border="1" align="center" cellpadding="8">
-    <tr>
-        <th>Processor</th>
-        <td>Core i5</td>
-    </tr>
-        <tr>
-        <th>Ram</th>
-        <td>8GB</td>
-    </tr>
-        <tr>
-        <th>Storage</th>
-        <td>128GB SSD</td>
-    </tr>
-        <tr>
-        <th>Size</th>
-        <td>0.59 inch</td>
-    </tr>
-        <tr>
-        <th>Weight</th>
-        <td>2.5 lbs</td>
-    </tr>
-</table><br/>
- <h3 style="text-align:center">Conclusion</h3>    
-    <p>If you want a laptop that soothes your personality, Avita Liber is the one which has been built by professionals from all over the world by keeping the customer requirements in their minds. 
-Just Go for it.</p>
-<br/>
-    
-        </div>
-        </div>
-        </div>
+
+
+
+       </div>
         </section>
+
         <div class="gotop-wrap">
             <button class="btn-gotop"><span class="sr-only">Back to Top</span></button>
         </div>
@@ -96,8 +80,8 @@ Just Go for it.</p>
 @section('js')
 
     <script>
-
-        $('.fb_share_btn').click( function(e) {
+    
+  $('.fb_share_btn').click( function(e) {
 
             e.preventDefault();
 
@@ -108,6 +92,7 @@ Just Go for it.</p>
 
             });
         });
+        
 
         $('.twitter_share_btn').click(function(e){
 
