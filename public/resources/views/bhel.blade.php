e233730df0141f10e5e18f7c3dd4ea7a0ff67006
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-06-08 17:33:28 +0530

    Create bhel.blade.php

---
+++ public/resources/views/bhel.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+    @lang('title.bhel')
+@stop
+
+
+@section('content')
+
+<main class="top-nav-padding">
+
+	<section class="">
+
+        <img src="/images/bhel.png" class="w-100 ">
+
+    </section>
+
+</main>
+
+@stop
