00508446d458ee3f91aa5928acb16268431137d1
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-06-13 17:39:03 +0530

    Update ProductsController.php

--- public/app/Http/Controllers/ProductsController.php
+++ public/app/Http/Controllers/ProductsController.php
@@ -133,7 +133,7 @@ public function showProductSpec( $slug) {
                 $join->on('shops.id', '=', 'shop_translations.shop_id');
                 $join->on('shop_translations.locale', '=', \DB::raw('"en"') );
             })
-                ->orderBy('shop_translations.name', 'asc');
+                ->orderBy('shop_translations.priority', 'asc');
 
         } else {
             $query = Shop::select();
