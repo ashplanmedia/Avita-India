934d665c4030fdb29c2995be95c99b1137228aed
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-07-11 14:05:48 +0530

    Update support.js

--- js/support.js
+++ js/support.js
@@ -48,7 +48,7 @@ $productTypeField.change(function() {
 
     if ( $(this).val() ) {
 
-        var url = "https://asp.avita.global/api/product/product-series?country=mu&brand=avita&type=1";
+        var url = "https://asp.avita.global/api/product/product-series?country=in&brand=avita&type=1";
 
         $.getJSON( url, {
             type: $(this).val()
@@ -92,7 +92,7 @@ $productSeriesField.change(function() {
     var select = $marketingNumberField;
 
     if ( $(this).val() ) {
-        var url = "https://asp.avita.global/api/product/marketing-numbers?country=mu&brand=avita&series=liber";
+        var url = "https://asp.avita.global/api/product/marketing-numbers?country=in&brand=avita&series=liber";
 
         $.getJSON( url, {
             series: $(this).val()
