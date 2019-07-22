1de2e5105157e79740e830ea4f274fdeb17cdbe7
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-07-22 14:01:51 +0530

    Update app.blade.php

--- public/resources/views/layouts/app.blade.php
+++ public/resources/views/layouts/app.blade.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<!----
-@php
-	$country = 'in';
-    $xml_lang = ($lang == 'en') ? 'en' : 'zh-hk';
 
-    $keyword = metaKeywordByCountryAndLanguage( $country, $lang);
 
-    $description = metaDescriptionByCountryAndLanguage( $country, $lang );
------->
-
-@endphp
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="{{ $xml_lang }}" lang="{{ $xml_lang }}">
 <head>
