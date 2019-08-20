b7523bd93f5528d9cf456c480478a5713bb4c960
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-08-20 11:05:52 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -69,21 +69,7 @@
                     </div>
                 </div>
             </div>
-                   <div class="carousel-item">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item d-flex align-items-center">
-                        <video class="leadin-video video-bgv2" muted playsinline>
-                            <source src="/videos/magus.mp4" type="video/mp4">
-                        </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'magus12-2in1-laptop') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
+       
             <ol class="carousel-indicators">
                 <li data-target="#home_banner" data-slide-to="0" class="active"></li>
                 <li data-target="#home_banner" data-slide-to="1"></li>
@@ -122,9 +108,9 @@
 
             clearTimeout( imageTimer );
 
-            $(this).find('video')[0].pause();
+            $(this).find('video')[3].pause();
 
-            var new_video = $(e.relatedTarget).find('video')[0];
+            var new_video = $(e.relatedTarget).find('video')[3];
 
             if ( new_video ) {
                 new_video.play();
