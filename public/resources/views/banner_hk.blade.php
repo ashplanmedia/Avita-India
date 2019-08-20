7a4136ddbc8c4ffdf0d63ef175de68b58faa8a84
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-08-20 11:04:27 +0530

    Update banner_hk.blade.php

--- public/resources/views/banner_hk.blade.php
+++ public/resources/views/banner_hk.blade.php
@@ -69,6 +69,20 @@
                     </div>
                 </div>
             </div>
+                   <div class="carousel-item">
+                <div class="responsive-block">
+                    <div class="banner-block responsive-item d-flex align-items-center">
+                        <video class="leadin-video video-bgv2" muted playsinline>
+                            <source src="/videos/magus.mp4" type="video/mp4">
+                        </video>
+                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
+                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
+                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'magus12-2in1-laptop') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
             <ol class="carousel-indicators">
                 <li data-target="#home_banner" data-slide-to="0" class="active"></li>
