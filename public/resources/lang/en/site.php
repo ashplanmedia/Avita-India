b68fb63c16d946dfd351d56f0507d34d18f313c4
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-09-09 11:50:50 +0530

    Update site.php

--- public/resources/lang/en/site.php
+++ public/resources/lang/en/site.php
@@ -91,7 +91,7 @@ return [
         'At AVITA we believe in the wonder and power of the individual. We make premium technology products that not only fit your lifestyle but are representations of you.',
         'The AVITA product series which ranges from PC, smart home and IoT devices has come to realization based on three core values: Liberty, Diversity and Individualism for an exceptional and highly personal experience to the new generation of tech users.',
         'Your ideas, passions, personal style and what makes you uniquely you inspires us. And we believe that your choice in technology shouldn\'t define you, it should reflect you.',
-        'Our team is comprised of talented individuals who, before starting up AVITA, worked in product development for some big name tech companies. It was their collective enthusiasm for outstanding design and meaningful technology that brought them together to form AVITA. You can learn more about the company behind AVITA here.',
+        'Our team is comprised of talented individuals who, before starting up AVITA, worked in product development for some big name tech companies. It was their collective enthusiasm for outstanding design and meaningful technology that brought them together to form AVITA. You can learn more about the company behind AVITA <a href="https://www.nexstgo.com/">here.</a>',
         '“Live it up” is more than a tagline to us, it’s a part of our ethos and company DNA.',
     ],
  'aboutus_about'                 => 'About AVITA',
