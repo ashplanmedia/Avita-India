d22c6d50fcf72e476a9c8648d0f3b11c0cd726be
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-09-13 17:47:15 +0530

    Update product.blade.php

--- public/resources/views/product.blade.php
+++ public/resources/views/product.blade.php
@@ -11,7 +11,7 @@
                 <div class="card">
                     <div class="card-body">
                         <form method="POST" action="/create">
-                            @csrf
+                          
                             <div class="form-group row">
                                 <label for="product_type"  class="col-md-4 col-form-label text-md-right">Color / Pattern </label>
                                 <div class="col-md-6">
