d0581226910222ead7e9a6c6a43d70c612119426
ASHPLAN MEDIA <47777192+ashplanmedia@users.noreply.github.com>
2019-09-13 17:30:46 +0530

    Update product.blade.php

--- public/resources/views/product.blade.php
+++ public/resources/views/product.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <main>
-    <div class="container-fluid">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
